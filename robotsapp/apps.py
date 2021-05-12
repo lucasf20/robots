08a375b1272cde2c9ca3be5b1e4dc607ec961a07
lucas ferreira <ferreira.lucas639@gmail.com>
2021-05-12 11:02:07 -0300

    first commit

---
+++ robotsapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RobotsappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'robotsapp'
---
+++ robotsapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Cliente(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nome_da_empresa = models.CharField(max_length=500)
+    cnpj = models.CharField(max_length=30)
+    data_de_cadastro = models.DateField()
+
+    def __str__(self):
+        return self.nome_da_empresa
+
+class Robo(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    campo1 = models.TextField()
+    campo2 = models.TextField()
+    campo3 = models.TextField()
