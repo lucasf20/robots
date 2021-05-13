c0c8b9e43d23f59800530979f5ff4b900033f3b2
Lucas Ferreira <ferreira.lucas639@gmail.com>
2021-05-13 17:07:19 -0300

    custom fieldsets

--- robotsapp/admin.py
+++ robotsapp/admin.py
@@ -6,6 +6,14 @@ from .models import Cliente, Robo, Usuario
 # Register your models here.
 class MyUserAdmin(UserAdmin):
 
+    fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'fields':('username', 'password','first_name',
+             'last_name', 'email', 'cliente', 'groups', 'user_permissions', 'is_staff', 'is_superuser')
+        }),
+    )    
+
     add_fieldsets = (
         (None, {
             'classes':('wide',),
